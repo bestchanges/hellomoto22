18db65af8fb739df7c947c6b72844c5bf12d8559
Egor Fedorov <egor.fedorov@biznesoft.ru>
2017-12-10 00:22:13 +0300

    minor

--- server/update.sh
+++ server/update.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -v
 
 # run this on server side to update server to the latest
 . .bashrc
 git pull
 pip install -r requirements.txt
+sleep 4
 ./bestminer-server.sh stop
 python bestminer/distr.py
 ./bestminer-server.sh start

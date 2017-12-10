959ac3e792c574e7573de5a470089243b501c744
Egor Fedorov <egor.fedorov@biznesoft.ru>
2017-12-10 10:44:19 +0300

    add monitor

---
+++ server/monitor.sh
@@ -0,0 +1,10 @@
+while : ; do
+wget -q -O /dev/null -T 3 http://localhost:5000/auth/login
+ret=$?
+if [ "$ret" -ne "0" ] ; then
+	echo error
+	date
+	./bestminer-server.sh restart
+fi
+sleep 60
+done

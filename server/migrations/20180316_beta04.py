58e5788527527da138ebede76358b049eba002a3
Bestminer <bestminer@bestminer.io>
2018-03-23 20:28:34 +0000

    onserver

--- server/migrations/20180316_beta04.py
+++ server/migrations/20180316_beta04.py
@@ -3,10 +3,12 @@ from mongodb_migrations.base import BaseMigration
 class Migration (BaseMigration):
 
     def upgrade(self):
+        i = 1
         for config in self.db.configuration_group.find():
+            i += 1
             if 'user' in config and config['user']:
                 user = self.db.user.find_one({'_id' : config['user']})
-                config['code'] = user['email'] + '_' + config['name']
+                config['code'] = i
             else:
                 config['code'] = config['name']
             self.db.configuration_group.save(config)

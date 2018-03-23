f6ce660ec9447efbcea2db1ae8588731170a633f
Egor Fedorov <egor.fedorov@biznesoft.ru>
2018-03-24 01:29:44 +0300

    server

--- server/gunicorn.sh
+++
@@ -1,6 +0,0 @@
-BESTMINER_PLATFORM=production
-export BESTMINER_PLATFORM
- ~/.virtualenvs/bestminer/bin/gunicorn --bind 0.0.0.0:5000 -k gevent --daemon --pid gunicorn.sh.pid \
-	 --access-logfile log/access.log --error-logfile gunicorn.sh.err --capture-output \
-	 --graceful-timeout 7 bestminer:app
-
